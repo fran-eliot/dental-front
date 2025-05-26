452beebc20a7ca63152284f8e06451879d9d0fcd
AinhoaAlonso <ainhoaalonso88@gmail.com>
2025-05-26 11:25:37 +0200

    Estructura interna src

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'dental-front' title`, () => {
+  it(`should have the 'my-dental-app' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('dental-front');
+    expect(app.title).toEqual('my-dental-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, dental-front');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, my-dental-app');
   });
 });
---
+++ src/app/components/professionals-availabilities/professionals-availabilities.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-professionals-availabilities',
+  imports: [],
+  templateUrl: './professionals-availabilities.component.html',
+  styleUrl: './professionals-availabilities.component.css'
+})
+export class ProfessionalsAvailabilitiesComponent {
+
+}
