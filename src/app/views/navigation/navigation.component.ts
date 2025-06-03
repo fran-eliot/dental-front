2792fc31d3cdb766026b45bc42c06e0837fb70cd
AinhoaAlonso <ainhoaalonso88@gmail.com>
2025-06-04 00:12:27 +0200

    Modificacion Cierre de sesion y empiezo a dar estilos

--- src/app/views/navigation/navigation.component.ts
+++ src/app/views/navigation/navigation.component.ts
@@ -1,21 +1,27 @@
 import { Component } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { LoginService } from '../../service/login/login.service';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navigation',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, FormsModule, CommonModule],
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.css'
 })
 export class NavigationComponent {
-  
 
   constructor(
     private loginService:LoginService,
     private readonly router: Router
   ){}
+
+  //Verifica si el usuario esta loggeado
+  get isLoggedIn(): boolean {
+    return !!localStorage.getItem('access_token');
+  }
 
   //Cierre de sesión
   onlogOut(){
