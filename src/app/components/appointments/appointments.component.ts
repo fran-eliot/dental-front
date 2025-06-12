59e7c8ff2a91d0d04b03efa9bf8b83084d16bc2b
AinhoaAlonso <ainhoaalonso88@gmail.com>
2025-06-13 01:40:47 +0200

    front traer reservas

--- src/app/components/appointments/appointments.component.ts
+++ src/app/components/appointments/appointments.component.ts
@@ -1,12 +1,100 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AppointmentsService } from '../../service/appointment/appointments.service';
+import { Professional } from '../../model/Professional';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-appointments',
-  imports: [],
   standalone: true,
+  imports: [RouterModule, ReactiveFormsModule, FormsModule, CommonModule],
   templateUrl: './appointments.component.html',
-  styleUrl: './appointments.component.css'
+  styleUrls: ['./appointments.component.css']
 })
-export class AppointmentsComponent {
+export class AppointmentsComponent implements OnInit {
+  filterForm: FormGroup;
+  appointments: any[] = [];
+  appointmentsFiltradas: any[] = [];
+  professionales: Professional[] = [];
+  loading = false;
+  errorMsg = '';
 
+  constructor(
+    private fb: FormBuilder,
+    private appointmentsService: AppointmentsService,
+  ) {
+    this.filterForm = this.fb.group({
+      professional_id: ['', Validators.required],
+      date_appointments: ['', Validators.required],
+      turno: [''] // mañana, tarde, vacío (todos)
+    });
+  }
+
+  ngOnInit(): void {
+    this.loadProfessionals();
+
+    // Escucha los cambios del select turno
+    this.filterForm.get('turno')?.valueChanges.subscribe(() => {
+      this.filterByPeriod();
+    });
+  }
+
+  loadProfessionals() {
+    this.appointmentsService.getProfessionals().subscribe(
+      data => {
+        this.professionales = data;
+        console.log('Profesionales cargados:', this.professionales);
+      },
+      error => {
+        this.errorMsg = 'Error cargando profesionales. Intenta de nuevo.';
+      }
+    );
+  }
+
+  getAppointments() {
+    this.errorMsg = '';
+
+    if (this.filterForm.invalid) {
+      this.errorMsg = 'Por favor, selecciona profesional y fecha.';
+      return;
+    }
+
+    const { professional_id, date_appointments } = this.filterForm.value;
+
+    this.loading = true;
+    this.appointmentsService.getAppointments(professional_id, date_appointments)
+      .subscribe({
+        next: (res) => {
+          this.appointments = res;
+          this.filterByPeriod(); // Filtrar por turno después de cargar
+          this.loading = false;
+        },
+        error: () => {
+          this.errorMsg = 'Error cargando reservas. Intenta de nuevo.';
+          this.loading = false;
+        }
+      });
+  }
+
+  // Filtrar por turno (mañana / tarde)
+  filterByPeriod() {
+    const turnoSeleccionado = this.filterForm.value.turno?.trim().toLowerCase();
+
+    if (!turnoSeleccionado) {
+      this.appointmentsFiltradas = this.appointments; // Mostrar todos
+    } else {
+      this.appointmentsFiltradas = this.appointments.filter(app =>
+        app.periodo.trim().toLowerCase() === turnoSeleccionado
+      );
+    }
+  }
+
+  // Obtener hora de fin
+  getEndTime(startTime: string, duration: number): string {
+    const [hours, minutes, seconds] = startTime.split(':').map(Number);
+    const startDate = new Date();
+    startDate.setHours(hours, minutes + duration, seconds || 0);
+    return startDate.toTimeString().slice(0, 5); // HH:mm
+  }
 }
---
+++ src/app/model/Reserva.ts
@@ -0,0 +1,18 @@
+export class Reserva {
+  id_reservas: number;
+  patient_id: number;
+  professional_id: number;
+  treatments_id: number;
+  date_appointments: string;
+  time_appointments: string;
+  status_reservas: string;
+
+  constructor(id_reservas?: number, patient_id?: number, professional_id?: number, date_appointments?: string, time_appointments?: string, status_reservas?: string) {
+    this.id_reservas = id_reservas || 0;
+    this.professional_id = professional_id || 0;
+    this.date_appointments = date_appointments || '';
+    this.time_appointments = time_appointments || '';
+    this.patient_id = patient_id || 0;
+    this.status_reservas = status_reservas || 'PENDING';
+  }
+}
