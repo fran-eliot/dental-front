82c67502ba161457b41a8e87883c72cfa0c72a57
AlexKoji <alexdacruzsemiao@gmail.com>
2025-06-15 18:18:26 +0200

    Treatments

--- src/app/components/treatments/treatments.component.ts
+++ src/app/components/treatments/treatments.component.ts
@@ -1,11 +1,40 @@
 import { Component } from '@angular/core';
+import { Treatments } from '../../Model/treatments';
+import { TreatmentsService } from '../../service/treatments.Service';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-treatments',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './treatments.component.html',
   styleUrl: './treatments.component.css'
 })
 export class TreatmentsComponent {
+  treatments: Treatments[];
+  constructor(private treatmentsService: TreatmentsService) {}
+
+  ngOnInit(): void {
+    this.treatmentsService.buscarTreatments().subscribe({
+      next: (data) => {
+        this.treatments = data;
+      },
+      error: (err) => {
+        console.error('Error al cargar tratamientos:', err);
+      }
+    });
+  }
+  createTreatment(treatment: Treatments): void {
+    this.treatmentsService.createTreatment(treatment).subscribe({
+      next: (data) => {
+        console.log('Tratamiento creado:', data);
+        this.treatments.push(data); // Añadir el nuevo tratamiento a la lista
+      },
+      error: (err) => {
+        console.error('Error al crear tratamiento:', err);
+      }
+    });
+  }
+
 
 }
---
+++ src/app/service/treatments/Treatments.ts
@@ -0,0 +1,18 @@
+export class Treatments {
+  id_treatments: number;
+  name_treatments: string;
+  type_treatments: string;
+  duration_treatments: number;
+  price_treatments: number;
+  /*visible_to_patients_treatments: boolean;*/
+
+
+  constructor(id_treatments: number, name_treatments: string, type_treatments: string, duration_treatments: number, price_treatments: number, visible_to_patients_treatments: boolean) {
+    this.id_treatments = id_treatments;
+    this.name_treatments = name_treatments;
+    this.type_treatments = type_treatments;
+    this.duration_treatments = duration_treatments;
+    this.price_treatments = price_treatments;
+    /*this.visible_to_patients_treatments = visible_to_patients_treatments;*/
+  }
+}
