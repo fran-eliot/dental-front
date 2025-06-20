aeef525d19b42eda1c961f8b1235b2fa983b0676
AinhoaAlonso <ainhoaalonso88@gmail.com>
2025-06-20 12:24:06 +0200

    Estilos home, footer..

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './views/home/home.component';
 import { NavigationComponent } from './views/navigation/navigation.component';
+import { FooterComponent } from './views/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  standalone: true, 
-  imports: [RouterOutlet, HomeComponent, NavigationComponent],
+  standalone: true,
+  imports: [RouterOutlet, HomeComponent, NavigationComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
